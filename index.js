0dedaebc8c0dc7b25c07ed0d84ad5809131af73d
yodit melile <yoditmelile@gmail.com>
2023-12-22 10:16:35 +0300

    contact

--- index.js
+++ index.js
@@ -42,27 +42,38 @@ function updateDots(index) {
     });
 }
 
-document.getElementById('downloadContact').addEventListener('click', function() {
+// document.getElementById('downloadContact').addEventListener('click', function() {
     
-    var imageUrl = 'https://www.iconnect.plus/mohamed/header.png';
+//     var imageUrl = 'https://drive.google.com/file/d/1P8QmVzzTLYeTjpoWGSYAz7LhqAJ2N8fW/view?usp=sharing';
   
     
-    var vCardData = 'BEGIN:VCARD\nVERSION:3.0\nFN:Yonas Teklu\nTEL:0913118633\nPHOTO;VALUE=URI:' + imageUrl + '\nEND:VCARD';
+//     var vCardData = 'BEGIN:VCARD\nVERSION:3.0\nFN:Yonas Teklu\nTEL:0913118633\nPHOTO;VALUE=URI:' + imageUrl + '\nEND:VCARD';
   
   
-    var blob = new Blob([vCardData], { type: 'text/vcard;charset=utf-8' });
+//     var blob = new Blob([vCardData], { type: 'text/vcard;charset=utf-8' });
   
 
-    var a = document.createElement('a');
+//     var a = document.createElement('a');
   
    
-    a.download = 'contact.vcf';
-    a.href = window.URL.createObjectURL(blob);
+//     a.download = 'contact.vcf';
+//     a.href = window.URL.createObjectURL(blob);
   
    
-    document.body.appendChild(a);
-    a.click();
+//     document.body.appendChild(a);
+//     a.click();
   
    
-    document.body.removeChild(a);
-  });
+//     document.body.removeChild(a);
+//   });
+
+
+document.getElementById('downloadButton').addEventListener('click', function() {
+    var vCardFileUrl = 'https://vcf.fyi/5e955ac5';
+    var link = document.createElement('a');
+    link.href = vCardFileUrl;
+    link.download = 'Yonas Teklu.vcf';
+    document.body.appendChild(link);
+     link.click();
+    document.body.removeChild(link);
+});
